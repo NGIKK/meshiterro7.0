6d9c2976eb44b444ce346bf0bb466d74cef82d0e
NGIKK <129597259+NGIKK@users.noreply.github.com>
2023-04-06 07:37:41 +0000

    [Finish]4-10

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
+  root to: "homes#top"
+  devise_for :users
+  devise_scope :user do
+     get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
+  resources :post_images,only: [:new,:create,:index,:show]
+  
+  # get 'postimages/new'
+  # get 'postimages/index'
+  # get 'postimages/show'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+ 
+ get 'homes/about' =>'homes#about',as:'about'
+end
